c0675d886516f2d4fd042941b22accb2714e61f2
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:34:28 -0300

    Add files via upload

---
+++ EX-4-LISTA-1-Yohan_Aquino/EX-4-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_4_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*4. A partir dos valores da base e altura de um triângulo, calcular e exibir sua área.*/
+            Triangulo tri =new Triangulo();
+
+            Console.WriteLine("Digite a base do triângulo: ");
+            tri.setBase(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite a altura do triângulo: ");
+            tri.setAltura(double.Parse(Console.ReadLine()));
+
+            tri.calcularArea();
+            Console.WriteLine("A área do triângulo é {0}",tri.getArea());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EX-4-LISTA-1-Yohan_Aquino/EX-4-LISTA-1-Yohan_Aquino/Triangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_4_LISTA_1_Yohan_Aquino
+{
+    class Triangulo
+    {
+        private double b;
+        private double h;
+        private double a;
+
+        public void setBase(double x)
+        {
+            b = x;
+        }
+        public void setAltura(double x)
+        {
+            h = x;
+        }
+        public void calcularArea()
+        {
+            a = (b * h) / 2;
+        }
+        public double getArea()
+        {
+            return a;
+        }
+    }
+}
